466baa4b674e8bb2f2b96a12659e4d388fb39144
amagdas <magdasadrian@gmail.com>
2019-03-03 17:55:10 +0200

    FIX: update deps.

--- mix.exs
+++ mix.exs
@@ -2,40 +2,43 @@ defmodule Obelisk.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :obelisk,
-     version: "0.10.0",
-     elixir: "~> 1.0",
-     package: package,
-     docs: [readme: true, main: "README.md"],
-     description: """
-      obelisk is a static site generator for Elixir. It is inspired by jekyll,
-      with the goal of being fast and simple to use and extend.
-     """,
-     deps: deps]
+    [
+      app: :obelisk,
+      version: "0.10.0",
+      elixir: "~> 1.0",
+      package: package,
+      docs: [readme: true, main: "README.md"],
+      description: """
+       obelisk is a static site generator for Elixir. It is inspired by jekyll,
+       with the goal of being fast and simple to use and extend.
+      """,
+      deps: deps
+    ]
   end
 
   def application do
-    [applications: [:yamerl, :cowboy, :plug, :chronos],
-     mod: {Obelisk, []}]
+    [applications: [:yamerl, :cowboy, :plug, :chronos], mod: {Obelisk, []}]
   end
 
   defp deps do
-    [{:yamerl, github: "yakaz/yamerl"},
-     {:earmark, "~> 0.1.15"},
-     {:chronos, "~> 1.0.0"},
-     {:cowboy, "~> 1.0.0"},
-     {:plug, "~> 0.11.1"},
-     {:rss, "~> 0.2.1"},
-     {:anubis, "~> 0.1.0"},
-     {:mock, "~> 0.1.0"},
-     {:calliope, "~> 0.3.0"}]
+    [
+      {:yamerl, github: "yakaz/yamerl"},
+      {:earmark, "~> 1.3.1"},
+      {:chronos, "~> 1.8.0"},
+      {:cowboy, "~> 2.6.1"},
+      {:plug, "~> 1.7.2"},
+      {:rss, "~> 0.2.1"},
+      {:anubis, "~> 0.3.0"},
+      {:mock, "~> 0.3.3"},
+      {:calliope, "~> 0.4.2"}
+    ]
   end
 
   defp package do
     %{
       licenses: ["MIT"],
       contributors: ["Benny Hallett"],
-      links: %{ "Github" => "https://github.com/bennyhallett/obelisk"}
+      links: %{"Github" => "https://github.com/bennyhallett/obelisk"}
     }
   end
 end
