70accdc19521ab5e5870c9827fd32507fc733088
amagdas <magdasadrian@gmail.com>
2019-03-03 19:44:47 +0200

    FIX: use plug_cowboy

--- lib/obelisk/tasks/server.ex
+++ lib/obelisk/tasks/server.ex
@@ -8,11 +8,10 @@ defmodule Obelisk.Tasks.Server do
   """
 
   def run(_) do
-    Application.start(:cowboy)
-    Application.start(:plug)
+    Application.start(:plug_cowboy)
     IO.puts("Starting Cowboy server. Browse to http://localhost:4000/")
     IO.puts("Press <CTRL+C> <CTRL+C> to quit.")
-    {:ok, pid} = Plug.Adapters.Cowboy.http(Obelisk.Plug.Server, [])
+    {:ok, pid} = Plug.Cowboy.http(Obelisk.Plug.Server, [])
 
     :timer.sleep(:infinity)
   end
